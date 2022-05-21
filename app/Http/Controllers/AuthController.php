ddbc7bf5be561d2d0537cf6c5f7ee93d0f98ad12
Faizalqossam <alqassamf@gmail.com>
2022-05-21 15:05:56 +0700

    Initial commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(){
+        return view("register");
+    }
+
+    public function welcome(Request $request){
+        return view('welcome', [
+            'firstname' => $request->firstname,
+            'lastname' => $request->lastname
+        ]);
+    }
+}
